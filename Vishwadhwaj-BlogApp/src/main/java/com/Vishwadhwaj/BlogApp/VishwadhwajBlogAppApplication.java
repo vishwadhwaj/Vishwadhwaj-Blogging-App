05c642f782947f8c45af899de10be05564fe3556
Vishwadhwaj <kumarvishwadhwaj@gmail.com>
2022-12-16 15:43:42 +0530

    initial commit

---
+++ Vishwadhwaj-BlogApp/src/main/java/com/Vishwadhwaj/BlogApp/VishwadhwajBlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.Vishwadhwaj.BlogApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishwadhwajBlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VishwadhwajBlogAppApplication.class, args);
+	}
+
+}
